129e1a13e55101367931ad4da1978f0b64435306
maqweb <balikhinv@bk.ru>
2020-03-26 19:29:33 +0300

    +

--- build/js/scripts.min.js
+++ build/js/scripts.min.js
@@ -2,20 +2,6 @@ window.addEventListener("DOMContentLoaded", init);
 
 function init() {
 
-  // var navItem = document.getElementsByClassName("nav__item");
-  // var activeItem = document.getElementsByClassName("nav__item_active");
-  
-
-  // for (var i = 0; i < navItem.length; i++) {
-
-  //   navItem[i].addEventListener('click', function () {
-  //     this.classList.add('nav__item_active');
-
-  //   });
-  // }
-
-
-
 }
 
 
